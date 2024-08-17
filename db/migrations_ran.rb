dfadf9f6cece08daa4281c090d5fe9b5f0654098
Grant Davis <grantdavis303@gmail.com>
2024-08-16 19:28:29 -0600

    Update Migrations Ran

--- db/migrations_ran.rb
+++ db/migrations_ran.rb
@@ -5,4 +5,7 @@ rails g model User first_name last_name email phone_number
 rails g model Role name
 rails g model Account user:references role:references username password_digest last_login
 
+rails g model Drink account:references drink_type ounces:decimal percentage:decimal
+rails g controller Drinks
+
 rails db:migrate
