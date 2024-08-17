89af4281494b2976c7848da7bdb3f8a6c8f3cdcb
Grant Davis <grantdavis303@gmail.com>
2024-08-16 19:28:52 -0600

    Update Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,73 @@
-usr = Role.create!(name: 'user')
-adm = Role.create!(name: 'admin')
+user = Role.create!(name: 'user')
+admin = Role.create!(name: 'admin')
 
 user1 = User.create!(
-  first_name: 'Brandon',
-  last_name: 'Smith',
-  email: 'test1@test.com',
-  phone_number: '123-456-789-1'
+  first_name: 'Grant',
+  last_name: 'Davis',
+  email: 'test@test.com',
+  phone_number: '1234567890',
 )
 
-user2 = User.create!(
-  first_name: 'Kyle',
-  last_name: 'Tucker',
-  email: 'test2@test.com',
-  phone_number: '123-456-789-2'
-)
-
-Account.create!(
+account1 = Account.create!(
   user_id: user1.id,
-  role_id: usr.id,
-  username: 'bsmith876',
-  password: 'Brandonsmithpw123!',
-  last_login: '1/24/2024'
+  role_id: user.id,
+  username: 'testuser1',
+  password: 'Password123!',
+  last_login: 'Does not matter',
 )
 
-Account.create!(
-  user_id: user2.id,
-  role_id: adm.id,
-  username: 'ktuck123',
-  password: 'Ktuckpw123!',
-  last_login: '1/24/2024'
+account1.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '16',
+  percentage: '8.1'
+)
+
+account1.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '12',
+  percentage: '8'
+)
+
+account1.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '16',
+  percentage: '7.2'
+)
+
+account1.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '12',
+  percentage: '6.5'
+)
+
+account1.drinks.create!(
+  drink_type: 'Wine',
+  ounces: '12.7',
+  percentage: '13.5'
+)
+
+account1.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '16',
+  percentage: '7'
+)
+
+account1.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '16',
+  percentage: '6.95'
+)
+
+account1.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '12',
+  percentage: '8'
+)
+
+account1.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '16',
+  percentage: '7.3'
 )
 
 puts "Seeded Successfully"
