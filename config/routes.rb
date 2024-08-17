0a3d69c3b24cd91808b9972a72ead19012bbaab3
Grant Davis <grantdavis303@gmail.com>
2024-08-16 19:29:14 -0600

    Update Routes w/ UsernameReset and Drinks

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :user_accounts, only: [:new, :create]
+  resources :username_reset, only: [:index]
   resources :password_reset, only: [:index]
 
   resources :dashboard, only: [:index]
+
+  resources :drinks, only: [:index, :new, :create]
 
   get '*path' => redirect('/')
   get 'up' => 'rails/health#show', as: :rails_health_check
