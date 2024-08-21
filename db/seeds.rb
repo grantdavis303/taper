3b8155895039b71696141ccd09acc57d9e7c1449
Grant Davis <grantdavis303@gmail.com>
2024-08-21 10:03:52 -0600

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -106,4 +106,11 @@ account.drinks.create!(
   created_at: 'Sun, 18 Aug 2024 18:00:00.000000000 UTC +00:00'
 )
 
+account.drinks.create!(
+  drink_type: 'Beer',
+  ounces: '12',
+  percentage: '5',
+  created_at: 'Tue, 20 Aug 2024 12:00:00.000000000 UTC +00:00'
+)
+
 puts "Seeded Successfully"
