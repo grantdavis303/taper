7e68d0a77f16f63ebcdb87540f8e45c8759609e7
Grant Davis <grantdavis303@gmail.com>
2024-09-03 10:05:28 -0600

    Create an “About” section on Sessions New

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root "sessions#new"
 
+  get '/about', to: 'static#about'
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :user_accounts, only: [:new, :create]
   resources :username_reset, only: [:index]
   resources :password_reset, only: [:index]
 
   resources :dashboard, only: [:index]
-
   resources :drinks, only: [:index, :new, :create, :destroy]
 
   get '*path' => redirect('/')
