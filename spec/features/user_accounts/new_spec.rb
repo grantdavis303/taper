2394eac76f92d02a7a93e72b4547f697ca482af1
Grant Davis <grantdavis303@gmail.com>
2024-09-24 14:14:00 -0600

    Add New Feature Tests for UserAccount

--- spec/features/user_accounts/new_spec.rb
+++ spec/features/user_accounts/new_spec.rb
@@ -30,6 +30,183 @@ RSpec.describe 'UserAccount New', type: :feature do
     end
   end
 
-  xit 'successfully creates a new user_account' do
+  it 'successfully creates a new user_account if all fields are valid' do
+    Role.create!(id: 1, name: 'user')
+
+    expect(User.all.count).to eq (0)
+    expect(Account.all.count).to eq (0)
+
+    visit new_user_account_path
+
+    within '.new_user_container' do
+      fill_in (:first_name), with: 'Bob the Test Bot'
+      fill_in (:last_name), with: 'Testerman'
+      fill_in (:email), with: 'bob_the_test_bot@bot_test.com'
+      fill_in (:phone_number), with: '123-456-789'
+      fill_in (:username), with: 'bob_the_test_bot123'
+      fill_in (:password), with: 'Password123!'
+      fill_in (:password_confirmation), with: 'Password123!'
+      click_on('Create Account')
+    end
+
+    expect(current_path).to eq (root_path)
+    expect(page).to have_content('Account created successfully.')
+    expect(User.all.count).to eq (1)
+    expect(Account.all.count).to eq (1)
+  end
+
+  it 'does not create a new user_account if the email is already taken' do
+    Role.create!(id: 1, name: 'user')
+    
+    user = User.create!(
+      first_name: 'Bob the Test Bot',
+      last_name: 'Testerman',
+      email: 'bob_the_test_bot@bot_test.com',
+      phone_number: '123-456-789',
+    )
+    
+    account = Account.create!(
+      user_id: user.id,
+      role_id: 1,
+      username: 'bob_the_test_bot123',
+      password: 'Password123!',
+      created_at: 'Thur, 8 Aug 2024 12:00:00.000000000 UTC +00:00'
+    )
+
+    expect(User.all.count).to eq (1)
+    expect(Account.all.count).to eq (1)
+
+    visit new_user_account_path
+
+    within '.new_user_container' do
+      fill_in (:first_name), with: 'Bob the Test Bot'
+      fill_in (:last_name), with: 'Testerman'
+      fill_in (:email), with: 'bob_the_test_bot@bot_test.com'
+      fill_in (:phone_number), with: '123-456-789'
+      fill_in (:username), with: 'bob_the_test_bot123'
+      fill_in (:password), with: 'Password123!'
+      fill_in (:password_confirmation), with: 'Password123!'
+      click_on('Create Account')
+    end
+
+    expect(current_path).to eq (new_user_account_path)
+    expect(page).to have_content('Email has already been taken.')
+    expect(User.all.count).to eq (1)
+    expect(Account.all.count).to eq (1)
+  end
+
+  it 'does not create a new user_account if the username is already taken' do
+    Role.create!(id: 1, name: 'user')
+    
+    user = User.create!(
+      first_name: 'Bob the Test Bot',
+      last_name: 'Testerman',
+      email: 'bob_the_test_bot@bot_test.com',
+      phone_number: '123-456-789',
+    )
+    
+    account = Account.create!(
+      user_id: user.id,
+      role_id: 1,
+      username: 'bob_the_test_bot123',
+      password: 'Password123!',
+      created_at: 'Thur, 8 Aug 2024 12:00:00.000000000 UTC +00:00'
+    )
+
+    expect(User.all.count).to eq (1)
+    expect(Account.all.count).to eq (1)
+
+    visit new_user_account_path
+
+    within '.new_user_container' do
+      fill_in (:first_name), with: 'Bob the Test Bot'
+      fill_in (:last_name), with: 'Testerman'
+      fill_in (:email), with: 'bob_the_test_bot2@bot_test.com'
+      fill_in (:phone_number), with: '123-456-789'
+      fill_in (:username), with: 'bob_the_test_bot123'
+      fill_in (:password), with: 'Password123!'
+      fill_in (:password_confirmation), with: 'Password123!'
+      click_on('Create Account')
+    end
+
+    expect(current_path).to eq (new_user_account_path)
+    expect(page).to have_content('Username has already been taken.')
+    expect(User.all.count).to eq (1)
+    expect(Account.all.count).to eq (1)
+  end
+
+  it 'does not create a new user_account if the email is empty' do
+    Role.create!(id: 1, name: 'user')
+
+    expect(User.all.count).to eq (0)
+    expect(Account.all.count).to eq (0)
+
+    visit new_user_account_path
+
+    within '.new_user_container' do
+      fill_in (:first_name), with: 'Bob the Test Bot'
+      fill_in (:last_name), with: 'Testerman'
+      fill_in (:email), with: ''
+      fill_in (:phone_number), with: '123-456-789'
+      fill_in (:username), with: 'bob_the_test_bot123'
+      fill_in (:password), with: 'Password123!'
+      fill_in (:password_confirmation), with: 'Password123!'
+      click_on('Create Account')
+    end
+
+    expect(current_path).to eq (new_user_account_path)
+    expect(page).to have_content("Email can't be blank and Email is not valid.")
+    expect(User.all.count).to eq (0)
+    expect(Account.all.count).to eq (0)
+  end
+
+  it 'does not create a new user_account if the password is empty' do
+    Role.create!(id: 1, name: 'user')
+
+    expect(User.all.count).to eq (0)
+    expect(Account.all.count).to eq (0)
+
+    visit new_user_account_path
+
+    within '.new_user_container' do
+      fill_in (:first_name), with: 'Bob the Test Bot'
+      fill_in (:last_name), with: 'Testerman'
+      fill_in (:email), with: 'bob_the_test_bot@bot_test.com'
+      fill_in (:phone_number), with: '123-456-789'
+      fill_in (:username), with: 'bob_the_test_bot123'
+      fill_in (:password), with: ''
+      fill_in (:password_confirmation), with: ''
+      click_on('Create Account')
+    end
+
+    expect(current_path).to eq (new_user_account_path)
+    expect(page).to have_content("Password can't be blank, Password is too short (minimum is 8 characters), and Password is missing all required characters.")
+    expect(User.all.count).to eq (0)
+    expect(Account.all.count).to eq (0)
+  end
+
+  it 'does not create a new user_account if the passwords do not match' do
+    Role.create!(id: 1, name: 'user')
+
+    expect(User.all.count).to eq (0)
+    expect(Account.all.count).to eq (0)
+
+    visit new_user_account_path
+
+    within '.new_user_container' do
+      fill_in (:first_name), with: 'Bob the Test Bot'
+      fill_in (:last_name), with: 'Testerman'
+      fill_in (:email), with: 'bob_the_test_bot@bot_test.com'
+      fill_in (:phone_number), with: '123-456-789'
+      fill_in (:username), with: 'bob_the_test_bot123'
+      fill_in (:password), with: 'Password123!'
+      fill_in (:password_confirmation), with: 'Password124!'
+      click_on('Create Account')
+    end
+
+    expect(current_path).to eq (new_user_account_path)
+    expect(page).to have_content('These passwords do not match.')
+    expect(User.all.count).to eq (1)
+    expect(Account.all.count).to eq (0)
   end
 end
